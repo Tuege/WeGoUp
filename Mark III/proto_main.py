462ddf67895baf3a73ccb0c48ae980fda32f6b81
Tuege <47752280+Tuege@users.noreply.github.com>
2022-11-06 14:49:55 +0100

    fixed local imports with Classed based selection between live and backtesting

--- Mark III/proto_main.py
+++ Mark III/proto_main.py
@@ -6,25 +6,38 @@ import numpy as np
 import yfinance as yf
 
 import threading
-from wgu3_gan import gan as prediction
+del sys.modules["wgu3_gan.gan"]
+del wgu3_gan.gan
+from wgu3_gan import gan
+#del sys.modules["moduleName"]
+#del moduleName
+#import wgu3_gan.gan
+#from wgu3_gan import gan
 #import wgu3_gan
 #from wgu3_gan.gan import live
+
+#print(dir(wgu3_gan.gan))
+
 
 if backtesting:
     pass
     #import wgu3_gan.gan_bt      as prediction
+    prediction = gan.GanBT()
     #from wgu3_profit_risk       import profit_risk_bt   as feasibility
     #from wgu3_strategyEngine    import algorithm_bt     as strategy
 else:
     pass
     #import wgu3_gan.gan         as prediction
+    prediction = gan.Gan()
     #from wgu3_profit_risk       import profit_risk      as feasibility
     #from wgu3_strategyEngine    import algorithm        as strategy
 
 
 print("Hello World!")
-p = prediction.live()
-print(p.run())
+prediction.run()
+#p = gan.backtesting()
+#print(p.run())
 
 #%%
 'New execution section'
+
--- Mark III/wgu3_gan/gan.py
+++
@@ -1,7 +0,0 @@
-
-class live():
-    def __init__(self):
-        print("live initialised")
-
-    def run(self):
-        print("run executed")
