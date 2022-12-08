12991cacd7a8a2606f24c6a96f2bb1b347969c4d
Tuege <47752280+Tuege@users.noreply.github.com>
2022-12-08 14:05:28 +0100

    working first prototype of LSTM with 6 features. no batches and only partial dataset

--- Mark III/wgu3_prediction_engine/GAN_TEST_FILE.py
+++ Mark III/wgu3_prediction_engine/GAN_TEST_FILE.py
@@ -2,8 +2,10 @@ import multiprocessing as mp
 import os
 import sys
 import time
+import importlib
 
 from DCGAN import dcgan as gan
+import DCGAN
 
 
 def count():
@@ -13,13 +15,21 @@ def count():
             pass
 
 
+def train_gan():
+    """gan.trainer(params)
+    gan.optimiser(params)
+    gan.hyperparameter_optimiser(params)
+    gan.train(dataset, hyperparameter_optimisation=True)"""
+    gan.train()
+
+
 if __name__ == '__main__':
     mp.freeze_support()
 
     gan_thread = mp.Process(target=gan.train)
     other_thread = mp.Process(target=count)
-    gan_thread.start()
-    other_thread.start()
+    # gan_thread.start()
+    # other_thread.start()
 
 
 
